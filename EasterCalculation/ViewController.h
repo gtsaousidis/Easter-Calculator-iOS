0fbfb2cc4eccc772fd6572ea6d526b638e105cb8
George Tsaousidis <georgetsd@MacBook-Pro-toutes-George.local>
2015-03-12 19:25:41 +0200

    Initial Commit

---
+++ EasterCalculation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasterCalculation
+//
+//  Created by George Tsaousidis on 12/3/15.
+//  Copyright (c) 2015 DFG-Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasterCalculation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasterCalculation
+//
+//  Created by George Tsaousidis on 12/3/15.
+//  Copyright (c) 2015 DFG-Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
